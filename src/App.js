f29f123a08ffe7046bf1629db93e516b83e5eca9
subin1848 <s2315@e-mirim.hs.kr>
2025-06-16 20:42:48 +0900

    ✨ [feat] 프레임과 버튼 UI 틀 구성 완료

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Shoot from "./pages/shoot/shoot";
 import Loading from "./pages/song/loading";
 import Song from "./pages/song/song";
 import Sketch from "./pages/detail/sketch" 
+import Home from "./pages/home";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Home />}></Route>
                 <Route path="/shoot" element={<Shoot />} />
                 <Route path="/loading" element={<Loading />} />
                 <Route path="/song" element={<Song />} />
